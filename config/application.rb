7fabd8e63ad5d626ad38919a279d2ca33f83490a
Emilio Bernal <ebernalzubieta@gmail.com>
2022-03-15 09:54:47 -0600

    Added CORS

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Backend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.hosts = ["localhost", "https://periferal-internacional.github.io/frontend"]
   end
 end
